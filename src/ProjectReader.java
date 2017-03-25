226f548d34099dccf495894a496a1a2a8e568656
Lixue_jiu <Lixue9jiu@users.noreply.github.com>
2017-03-25 00:04:48 -0400

    完成ProjectReader

--- src/ProjectReader.java
+++ src/ProjectReader.java
@@ -1,32 +1,122 @@
-import java.io.*;
-import javax.security.auth.*;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
-public class ProjectReader implements Destroyable
-{
-	private File m_file;
-	
-	public ProjectReader(File file){
-		m_file = file;
-	}
-	
-	public boolean load(World.Option opt){
-		return true;
-	}
-	
-	public File save(){
-		return m_file;
-	}
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-	@Override
-	public void destroy() throws DestroyFailedException
-	{
-		// TODO: Implement this method
-	}
+import javax.security.auth.DestroyFailedException;
+import javax.security.auth.Destroyable;
 
-	@Override
-	public boolean isDestroyed()
-	{
-		// TODO: Implement this method
-		return false;
-	}
+/**
+ * 使用PULL解析器解释Project.xml
+ */
+public class ProjectReader implements Destroyable {
+    private boolean isAvaliable;
+    private XmlPullParserFactory xmlFactory;
+
+    private File m_file;
+    private FileInputStream m_stream;
+
+    private GameInfo gameInfo;
+
+    public ProjectReader(File file) {
+        isAvaliable = true;
+        m_file = file;
+    }
+
+    public boolean load(World.Option opt) {
+        try {
+            m_stream = new FileInputStream(m_file);
+
+            xmlFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = xmlFactory.newPullParser();
+            parser.setInput(m_stream, "UTF-8");
+            int event = parser.getEventType();
+            String fatherNode = "";
+            boolean isChangingFather = true;
+            while (event != XmlPullParser.END_DOCUMENT) {
+                switch (event) {
+                    case XmlPullParser.START_DOCUMENT:
+                        gameInfo = new GameInfo();
+                        break;
+                    case XmlPullParser.START_TAG:
+                        if (isChangingFather) {
+                            fatherNode = parser.getAttributeValue(0);
+                            isChangingFather = false;
+                        }
+                        if ("GameInfo".equals(fatherNode)) {
+                            gameInfo.setValue(parser);
+                        }else if ("Player".equals(fatherNode) && "SpawnPosition".equals(parser.getAttributeValue(0))) {
+                            String[] str = parser.getAttributeValue(2).split(",");
+                            opt.origin = new Point(((int) Float.parseFloat(str[0])) / 16, ((int) Float.parseFloat(str[2])) / 16);
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if (fatherNode.equals(parser.getAttributeValue(0))) {
+                            isChangingFather = true;
+                        }
+                        break;
+                }
+                parser.next();
+            }
+        } catch (Exception e) {
+            isAvaliable = false;
+            e.printStackTrace();
+        }
+        return isAvaliable;
+    }
+
+    public File save() {
+        return m_file;
+    }
+
+    @Override
+    public void destroy() throws DestroyFailedException {
+        try {
+            m_stream.close();
+            isAvaliable = false;
+        } catch (IOException e) {
+            throw new DestroyFailedException(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return isAvaliable;
+    }
+
+    public class GameInfo {
+        public int WorldSeed;
+        public String WorldName;
+        public String TerrainGenerationMode;
+        public int TerrainLevel;
+        public int TerrainBlockIndex;
+        public int TerrainOceanBlockIndex;
+        public int TemperatureOffset;
+        public int HumidityOffset;
+        public int SeaLevelOffset;
+
+        public void setValue(XmlPullParser parser) {
+            if ("WorldSeed".equals(parser.getAttributeValue(0))) {
+                WorldSeed = Integer.parseInt(parser.getAttributeValue(2));
+            }else if ("WorldName".equals(parser.getAttributeValue(0))) {
+                WorldName = parser.getAttributeValue(2);
+            }else if ("TerrainGenerationMode".equals(parser.getAttributeValue(0))) {
+                TerrainGenerationMode = parser.getAttributeValue(2);
+            }else if ("TerrainLevel".equals(parser.getAttributeValue(0))) {
+                TerrainLevel = Integer.parseInt(parser.getAttributeValue(2));
+            }else if ("TerrainBlockIndex".equals(parser.getAttributeValue(0))) {
+                TerrainBlockIndex = Integer.parseInt(parser.getAttributeValue(2));
+            }else if ("TerrainOceanBlockIndex".equals(parser.getAttributeValue(0))) {
+                TerrainOceanBlockIndex = Integer.parseInt(parser.getAttributeValue(2));
+            }else if ("TemperatureOffset".equals(parser.getAttributeValue(0))) {
+                TemperatureOffset = Integer.parseInt(parser.getAttributeValue(2));
+            }else if ("HumidityOffset".equals(parser.getAttributeValue(0))) {
+                HumidityOffset = Integer.parseInt(parser.getAttributeValue(2));
+            }else if ("SeaLevelOffset".equals(parser.getAttributeValue(0))) {
+                SeaLevelOffset = Integer.parseInt(parser.getAttributeValue(2));
+            }
+        }
+    }
 }
